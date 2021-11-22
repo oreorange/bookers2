16b308170602714429ff6cb044b8b7770c539c51
EC2 Default User <ec2-user@ip-172-31-46-105.ec2.internal>
2021-11-22 12:49:08 +0000

    [Update2]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,6 +5,7 @@ class BooksController < ApplicationController
   def index
     @book = Book.new
     @books = Book.all
+    @user = current_user
     
   end
 
@@ -16,15 +17,20 @@ class BooksController < ApplicationController
   end
 
   def show
+    @book = Book.find(params[:id])
+  
   end
 
   def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to books_path
   end
   
   private
   
   def book_params
-   params.require(:book).permit(:title, :body)
+   params.require(:book).permit(:title, :body, :user_id)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   resources :books, only: [:new, :create, :index, :show, :destroy]
   
   
-  resources :users, only: [:show]
+  resources :users, only: [:show, :edit, :update, ]
 
 end
